4576e358b2503c138ba580d1d8348f548553e2bd
Varshavt1999 <varshavt7399@gmail.com>
2022-05-11 17:23:33 +0530

    textutils-projext-started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import About from './Components/About';
+import NavBar from './Components/NavBar';
+import TextForm from './Components/TextForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <>
+           <NavBar title="Text Utils" about="About Us"/> 
+           <TextForm heading="Enter The Text To Analyze below" />
+           <About />
+        </>
+        );
+    }
 
 export default App;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+function TextForm(props) {
+    const [text, setText] = useState("Enter Text Here")
+    const handleUpClick = () =>{
+        console.log("uppercase was clicked");
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+
+    const handleLoClick = () =>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleClearClick = () =>{
+        let newText = "";
+        setText(newText)
+    }
+
+
+    const handleOnChange = (e) =>{
+        console.log("on change"+text);
+        setText(e.target.value)
+    }
+    return (
+        <>
+            <div className='container my-5'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} id="TextBox" rows="8" onChange={handleOnChange}></textarea>
+                </div>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleUpClick}>Convert To Uppercase</button>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleLoClick}>Convert To Lowercase</button>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleClearClick}>Clear Text</button>
+            </div>
+            <div className='container my-3'>
+                <h3>Your Text Summery</h3>
+                <p>{text.split(" ").length} word and {text.length} characters</p>
+                <p>{ 0.008 * text.split(" ").length } Minutes read</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+            </div>
+        </>
+    )
+}
+
+export default TextForm    
